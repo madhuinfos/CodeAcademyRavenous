7b67f7d16bb9d3883c49cf594155e75b03a276f4
Sanisetty, Madhusudhan <madhusudhan.sanisetty@zeiss.com>
2018-02-03 21:38:15 +0530

    Ravenous first version

---
+++ src/components/Business/Business.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './Business.css';
+
+
+class Business extends React.Component{
+
+  render(){
+    return(
+      <div className="Business">
+        <div className="image-container">
+          <img src='https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg' alt=''/>
+        </div>
+        <h2>MarginOtto Pizzeria</h2>
+        <div className="Business-information">
+          <div className="Business-address">
+            <p>{this.props.business.address}</p>
+            <p>{this.props.business.city}</p>
+            <p>{this.props.business.state} {this.props.business.zipCode}</p>
+          </div>
+          <div className="Business-reviews">
+            <h3>{this.props.business.category}</h3>
+            <h3 className="rating">{this.props.business.rating} stars</h3>
+            <p>{this.props.business.reviewCount} reviews</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default Business;
+
+Business.propTypes = {
+  business: PropTypes.shape({
+    address: PropTypes.string.isRequired
+  }).isRequired
+};
---
+++ src/components/BusinessList/BusinessList.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './BusinessList.css';
+import Business from '../Business/Business.js';
+
+class BusinessList extends React.Component{
+  render(){
+    const x = this.props.businesses.map((business, i) => {
+      return <Business key = {i} business = {business} />
+    });
+    return (
+      <div className="BusinessList">
+      {
+        x
+      }
+
+      </div>
+    );
+  }
+}
+
+export default BusinessList;
